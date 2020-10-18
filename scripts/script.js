23b77fe58acdbec736c2201cb4e8aeb9dbf9b412
JosetteDerksen <70659177+JosetteDerksen@users.noreply.github.com>
2020-10-18 18:04:07 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+
+function myFunction() {
+    var x = document.getElementById("ul.links");
+    if (x.style.display === "block") 
+        {x.style.display = "none";}
+    else {x.style.display = "block"}
+}
+
+var menuKnop =
+document.querySelector("section.container");
+
+menuKnop.addEventListener("click", kruis);
+function kruis() {
+
+    menuKnop.classList.toggle("kruis");
+}
+
